447f5ad6937e75904f8d0816692933a7c7d443ad
xx <1804113857@qq.com>
2023-08-23 16:48:27 +0800

    resorder中dao层

---
+++ res-order/src/main/java/com/yc/resfood/dao/ResorderDao.java
@@ -0,0 +1,10 @@
+package com.yc.resfood.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yc.ResOrder;
+
+/**
+ * 订单操作:订单保存
+ */
+public interface ResorderDao extends BaseMapper<ResOrder> {
+}
---
+++ res-order/src/main/java/com/yc/resfood/dao/ResorderItemDao.java
@@ -0,0 +1,10 @@
+package com.yc.resfood.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yc.ResOrderItem;
+
+/**
+ * 订单详情操作 插入
+ */
+public interface ResorderItemDao extends BaseMapper<ResOrderItem> {
+}
