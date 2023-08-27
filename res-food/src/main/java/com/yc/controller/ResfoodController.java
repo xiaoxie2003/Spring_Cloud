d63f806d035e2f135ab07b667fd66c21a05c8e90
xx <1804113857@qq.com>
2023-08-27 19:28:38 +0800

    增加了异常测试

--- res-food/src/main/java/com/yc/controller/ResfoodController.java
+++ res-food/src/main/java/com/yc/controller/ResfoodController.java
@@ -1,5 +1,7 @@
 package com.yc.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yc.ResFood;
 import com.yc.biz.ResFoodBiz;
@@ -12,15 +14,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 @RestController
 @RequestMapping("resfood") //http:localhost:port/resfood
 @Slf4j
-@Api(tags = "菜品管理")
+@Api(tags = "菜品管理")  //swagger网页监控
 public class ResfoodController {
 
     @Autowired
@@ -54,6 +53,15 @@ public class ResfoodController {
         return map;
     }
 
+//    private Map<String,Object>handleBlock(@RequestParam int pageno, @RequestParam int pagesize,
+//                                          @RequestParam String sortby, @RequestParam String sort, BlockException exception){
+//        exception.printStackTrace();
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("code",0);
+//        map.put("msg",exception.getRuleLimitApp() + ":被限流，规则为：" + exception.getRule().toString());
+//        return map; //sentinel 200
+//    }
+
     @RequestMapping(value = "findByPage",method = {RequestMethod.GET,RequestMethod.POST})
     @ApiOperation(value = "分页查询操作")
     @ApiImplicitParams({
@@ -62,6 +70,7 @@ public class ResfoodController {
             @ApiImplicitParam(name = "sortby",value = "排序列",required = true),
             @ApiImplicitParam(name = "sort",value = "排序方式",required = true),
     })
+    //@SentinelResource(value = "hotkey-page",blockHandler = "handleBlock") //流控资源名
     public Map<String, Object> findByPage(@RequestParam int pageno,@RequestParam int pagesize,
                                           @RequestParam String sortby,@RequestParam String sort){
         Map<String,Object> map = new HashMap<>();
@@ -103,9 +112,36 @@ public class ResfoodController {
         return map;
     }
 
+    //set集合 存线程
+    public Set<Thread> set = new HashSet<>();
+
+//    private Map<String,Object>exceptionFallback(Throwable t){
+//        t.printStackTrace();
+//        Map<String,Object> map = new HashMap<>();
+//        map.put("code",0);
+//        map.put("msg","resource is under Exception cause as following：" + t.getCause());
+//        return map; //sentinel 200
+//    }
+
     @GetMapping("findAll")
     @ApiOperation(value = "查询所有菜品")
+    //@SentinelResource(value = "findAll",fallback = "exceptionFallback")
     public Map<String, Object> findAll(){
+//        int flag = 1;
+//        if(flag == 1){
+//            throw new RuntimeException("业务异常：查找出异常了...."); //springboot处理
+//        }
+
+//        Thread thread = Thread.currentThread(); //取出当前线程
+//        set.add(thread);
+//        log.info("线程数为：" + set.size() + ",当前的线程编号为：" + thread.getId());
+//
+//        try {
+//            Thread.sleep(20000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+
         Map<String,Object> map = new HashMap<>();
         List<ResFood> list = null;
         try {
