666efa27bef7a64567507b22929171608eca036f
xx <1804113857@qq.com>
2023-08-27 19:27:41 +0800

    配置中心更新

---
+++ res-food/src/main/java/com/yc/controller/ServiceController.java
@@ -0,0 +1,41 @@
+//package com.yc.controller;
+//
+//import io.swagger.annotations.Api;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.text.DateFormat;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//@RestController
+//@RequestMapping("service") //http:localhost:port/resfood
+//@Slf4j
+//@Api(tags = "服务")
+//@RefreshScope
+//public class ServiceController {
+//
+//    //利用DI将 res.pattern.dateformate注入
+//    //res:
+//    //  pattern:
+//    //    dateformat
+//    @Value("${res.pattern.dateformat}")
+//    private String dateFormate;
+//
+//    @RequestMapping("nowTime")
+//    public Map<String,Object> nowTime(){
+//        Map<String,Object> map = new HashMap<>();
+//
+//        Date d = new Date();
+//        DateFormat df = new SimpleDateFormat(dateFormate);
+//
+//        map.put("code",1);
+//        map.put("obj",df.format(d));
+//        return map;
+//    }
+//}
